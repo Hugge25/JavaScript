724049f45e01df5a67dcab9b433acda27b738715
Hugge25 <hugo.paulsson@hotmail.com>
2023-06-09 17:09:23 +0200

    js = ez

---
+++ code.js
@@ -0,0 +1,9 @@
+function Alert() {
+    alert("Hejsan svejsan!");
+  }
+
+function Text(){
+    const text = document.createElement("p");
+    text.innerText = "Hej på dig :)";
+    document.body.appendChild(text);
+}
